5dcd60a63a87f21dc714841a2c3685548d8638f3
Ibrahim-elyazidi <ibrahim.el-yazidi@lyceedecachan-sti.fr>
2023-03-23 16:18:16 +0100

    First commit

--- Symfony/Header.php
+++ Symfony/Header.php
@@ -3,11 +3,15 @@
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
+<link rel="stylesheet" href="Style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <nav class="navbar navbar-expand-lg bg-light">
+   
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php"><img src="../Images/Liberty.png" alt="LibertyLines_Logo" weight = 50 height = "50"> </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,9 +47,5 @@
     </div>
   </div>
 </nav>
-    
-</head>
-<body>
-    
 </body>
 </html>
